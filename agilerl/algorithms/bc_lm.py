Causal self attention flip for RL eval fix

agilerl/algorithms/bc_lm.py
@@ -174,7 +174,7 @@ class BC_Policy():
         logits, past_key_values = self.bc_lm(tokens=tokens, attn_mask=attn_mask, prefix_embs=prefix_embs, 
                                    prefix_attn_mask=prefix_attn_mask, 
                                    remove_prefix_position_embs=remove_prefix_position_embs,
-                                   is_casual=False)
+                                   is_causal=False)
         dialogue_kvs = past_key_values
         dialogue_lens = attn_mask.sum(dim=1)
         tokens = pad_sequence(torch.repeat_interleave(tokens, num_generations, dim=0), max_length, tokenizer.pad_token_id, device, 1)
@@ -186,7 +186,7 @@ class BC_Policy():
         while termination_mask.sum() > 0 and (t+prefix_t) < max_length:
             curr_token = tokens[:, t-1].unsqueeze(1)
             curr_dialogue_kvs = map_all_kvs(lambda x: x[:,:,:(t+prefix_t)-1,:], dialogue_kvs)
-            logits, past_key_values = self.bc_lm(curr_token, None, past_key_values=curr_dialogue_kvs, is_casual=False)
+            logits, past_key_values = self.bc_lm(curr_token, None, past_key_values=curr_dialogue_kvs, is_causal=False)
             logits[:, 0, tokenizer.pad_token_id] = torch.where(termination_mask == 1, float('-inf'), 1e7)
             logits[torch.arange(0, n).to(device), torch.full((n,), 0).to(device), tokens[:, t]] = logits[torch.arange(0, n).to(device), torch.full((n,), 0).to(device), tokens[:, t]].masked_fill_(t < dialogue_lens, 1e7)
             logits = process_logits(logits, temp=temp, top_k=top_k, top_p=top_p)
@@ -238,7 +238,7 @@ class BC_Policy():
         logits, past_key_values = self.bc_lm(tokens, attn_mask, prefix_embs=prefix_embs, 
                                    prefix_attn_mask=prefix_attn_mask, 
                                    remove_prefix_position_embs=remove_prefix_position_embs,
-                                   is_casual=False)
+                                   is_causal=False)
         dialogue_kvs = past_key_values
         original_dialogue_lens = attn_mask.sum(dim=1)
         batch_indicator = torch.stack(beam_width*[torch.arange(0, bsize).to(device)], dim=1)
@@ -251,7 +251,7 @@ class BC_Policy():
         while termination_mask.sum() > 0 and (t+prefix_t) < max_length:
             curr_token = tokens[:, t-1].unsqueeze(1)
             curr_dialogue_kvs = map_all_kvs(lambda x: x[:,:,:(t+prefix_t)-1,:], dialogue_kvs)
-            logits, past_key_values = self.bc_lm(curr_token, None, past_key_values=curr_dialogue_kvs, is_casual=False)
+            logits, past_key_values = self.bc_lm(curr_token, None, past_key_values=curr_dialogue_kvs, is_causal=False)
             logits[:, 0, tokenizer.pad_token_id] = torch.where(termination_mask == 1, float('-inf'), 1e7)
             logits[torch.arange(0, n).to(device), torch.full((n,), 0).to(device), tokens[:, t]] = logits[torch.arange(0, n).to(device), torch.full((n,), 0).to(device), tokens[:, t]].masked_fill_(t < dialogue_lens, 1e7)
             scores = (torch.log(F.softmax(logits, dim=-1)).reshape(1, bsize, beam_width, -1).permute(3, 0, 1, 2) + curr_scores).permute(1, 2, 3, 0).reshape(1, bsize, -1)  # (time, batch, k*vocab)

agilerl/algorithms/ilql.py
@@ -13,7 +13,6 @@ from agilerl.networks.evolvable_mlp import EvolvableMLP
 from agilerl.networks.evolvable_gpt import EvolvableGPT
 from agilerl.data.rl_data import DataPoint
 from agilerl.utils.sampling_utils import map_all_kvs, pad_sequence, update_kvs, process_logits, always_terminate
-from agilerl.utils.torch_utils import get_transformer_logs
 
 class ILQL(nn.Module):
     """The Implicit Language Q Learning algorithm class. ILQL paper: https://arxiv.org/pdf/2206.11871.pdf
@@ -571,7 +570,7 @@ class ILQL(nn.Module):
                                        skip_policy_on_train=(
                                            awac_weight == 0.0),
                                        )
-        tokens, attn_mask, model_outputs = get_qvs_outputs[
+        tokens, attn_mask, _ = get_qvs_outputs[
             'tokens'], get_qvs_outputs['attn_mask'], get_qvs_outputs['model_outputs']
         vs, qs = get_qvs_outputs['vs'], get_qvs_outputs['qs']
         vns, target_qs, rs = get_qvs_outputs['vns'], get_qvs_outputs['target_qs'], get_qvs_outputs['rs']
@@ -1000,7 +999,7 @@ class ILQL_Policy():
                                        prefix_embs=prefix_embs, 
                                        prefix_attn_mask=prefix_attn_mask, 
                                        remove_prefix_position_embs=remove_prefix_position_embs,
-                                       is_casual=False)['model_outputs']
+                                       is_causal=False)['model_outputs']
         kvs = {'qv': model_outputs['qv_model_outputs']['past_key_values']}
         if self.iql_model.actor_target is not None:
             kvs['target'] = model_outputs['target_model_outputs']['past_key_values']
@@ -1033,7 +1032,7 @@ class ILQL_Policy():
                                          qv_kwargs={'past_key_values': curr_kvs}, 
                                          policy_kwargs={'past_key_values': curr_policy_kvs}, 
                                          target_kwargs={'past_key_values': curr_target_kvs},
-                                         is_casual=False)
+                                         is_causal=False)
             model_outputs, logits = iql_outputs['model_outputs'], iql_outputs['logits']
             
             logits[:, 0, tokenizer.pad_token_id] = torch.where(termination_mask == 1, float('-inf'), 1e7)
@@ -1125,7 +1124,7 @@ class ILQL_Policy():
         model_outputs = self.iql_model(tokens, state_idxs, action_idxs, attn_mask,
                                        prefix_embs=prefix_embs, prefix_attn_mask=prefix_attn_mask,
                                        remove_prefix_position_embs=remove_prefix_position_embs,
-                                       is_casual=False)['model_outputs']
+                                       is_causal=False)['model_outputs']
         kvs = {'qv': model_outputs['qv_model_outputs']['past_key_values']}
         if self.iql_model.actor_target is not None:
             kvs['target'] = model_outputs['target_model_outputs']['past_key_values']
@@ -1162,7 +1161,7 @@ class ILQL_Policy():
                                          qv_kwargs={'past_key_values': curr_kvs}, 
                                          policy_kwargs={'past_key_values': curr_policy_kvs}, 
                                          target_kwargs={'past_key_values': curr_target_kvs}, 
-                                         is_casual=False)
+                                         is_causal=False)
             model_outputs, logits = iql_outputs['model_outputs'], iql_outputs['logits']
             
             logits[:, 0, tokenizer.pad_token_id] = torch.where(termination_mask == 1, float('-inf'), 1e7)

agilerl/networks/evolvable_gpt.py
@@ -156,7 +156,7 @@ class EvolvableGPT(nn.Module):
         net_dict['ln_f'] = LayerNorm(self.n_embd, bias=self.bias)
         return nn.ModuleDict(net_dict)
 
-    def forward(self, idx=None, tok_emb=None, targets=None, attn_mask=None, past_key_values=None, pos=None, is_casual=True):
+    def forward(self, idx=None, tok_emb=None, targets=None, attn_mask=None, past_key_values=None, pos=None, is_causal=True):
         """Forward pass through evolvable GPT model.
         
         :param idxs: Input ids
@@ -200,7 +200,7 @@ class EvolvableGPT(nn.Module):
             # Ensure layer_past is on same device as hidden_states (might not be correct)
             if layer_past is not None:
                 layer_past = tuple(past_state.to(x.device) for past_state in layer_past)
-            x, pres = block(x, attn_mask, layer_past, is_casual)
+            x, pres = block(x, attn_mask, layer_past, is_causal)
             all_hidden_states = all_hidden_states + (x,)
             presents = presents + (pres,)
         x = self.transformer.ln_f(x)
@@ -751,8 +751,8 @@ class Block(nn.Module):
         self.ln_2 = LayerNorm(n_embd, bias=bias, layer_norm_eps=layer_norm_eps)
         self.mlp = MLP(n_embd, dropout, hidden_size, activation)
 
-    def forward(self, x, attn_mask=None, layer_past=None, is_casual=True):
-        attn_output, present = self.attn(self.ln_1(x), attn_mask=attn_mask, layer_past=layer_past, is_casual=is_casual)
+    def forward(self, x, attn_mask=None, layer_past=None, is_causal=True):
+        attn_output, present = self.attn(self.ln_1(x), attn_mask=attn_mask, layer_past=layer_past, is_causal=is_causal)
         x = x + attn_output
         x = x + self.mlp(self.ln_2(x))
         return x, present