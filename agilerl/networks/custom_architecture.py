ec5b273e495c04b4d27d5de8ade6bf96b6370924
mikepratt1 <michael.pratt160@gmail.com>
2023-08-17 11:47:31 +0100

    added custom arch file with Gumbel Softmax in

---
+++ agilerl/networks/custom_architecture.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F
+
+class GumbelSoftmax(nn.Module):
+    """Applies gumbel softmax function element-wise
+    """
+    @staticmethod
+    def gumbel_softmax(logits, tau=1.0, eps=1e-20):
+        """Implementation of the gumbel softmax activation function
+        
+        :param logits: Tensor containing unnormalized log probabilities for each class.
+        :type logits: torch.Tensor
+        """
+        epsilon = torch.rand_like(logits)
+        logits += -torch.log(-torch.log(epsilon + eps) + eps)
+        return F.softmax(logits / tau, dim=-1)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return self.gumbel_softmax(input)
