658a91954625c12d9c0dc16cb554aa237bfc47fd
nicku-a <nickua@btinternet.com>
2023-10-10 15:37:07 +0100

    Apply js to top-level elements

--- docs/_static/js/expand_sidebar.js
+++ docs/_static/js/expand_sidebar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const checkboxes = document.querySelectorAll(".toctree-checkbox");
+    const checkboxes = document.querySelectorAll(".toctree-l1 > .toctree-checkbox");
     checkboxes.forEach(checkbox => {
         checkbox.checked = true;
     });
