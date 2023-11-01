9746dcfd436fdfdfde20417a10ac4bda0f4a23e7
nicku-a <nickua@btinternet.com>
2023-11-01 15:58:04 +0000

    Multi-agent algorithm tournament selection tests

--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -2,6 +2,8 @@ from agilerl.algorithms.cqn import CQN
 from agilerl.algorithms.ddpg import DDPG
 from agilerl.algorithms.dqn import DQN
 from agilerl.algorithms.dqn_rainbow import RainbowDQN
+from agilerl.algorithms.maddpg import MADDPG
+from agilerl.algorithms.matd3 import MATD3
 from agilerl.algorithms.ppo import PPO
 from agilerl.algorithms.td3 import TD3
 from agilerl.hpo.tournament import TournamentSelection
@@ -55,6 +57,7 @@ def test_initialization_with_given_parameters():
     assert ts.evo_step == evo_step
 
 
+### Single-agent algorithms ###
 # Returns best agent and new population of agents following tournament selection.
 def test_returns_best_agent_and_new_population():
     state_dim = [4]
@@ -129,6 +132,97 @@ def test_returns_best_agent_and_new_population_without_elitism():
         "PPO": PPO,
         "CQN": CQN,
     }
+
+    for algo in algo_classes.keys():
+        population = initialPopulation(
+            algo=algo,
+            state_dim=state_dim,
+            action_dim=action_dim,
+            one_hot=one_hot,
+            net_config=net_config,
+            INIT_HP=SHARED_INIT_HP,
+            population_size=population_size,
+            device=device,
+        )
+
+        population[0].fitness = [1, 2, 3]
+        population[1].fitness = [4, 5, 6]
+        population[2].fitness = [7, 8, 9]
+        population[3].fitness = [10, 11, 12]
+        population[4].fitness = [13, 14, 15]
+
+        # Call the select method
+        elite, new_population = tournament_selection.select(population)
+
+        # Check if the elite agent is the best agent in the population
+        assert elite.fitness == [13, 14, 15]
+        assert elite.index == 4
+
+        # Check if the new population has the correct length
+        assert len(new_population) == population_size
+
+
+### Multi-agent algorithms ###
+# Returns best agent and new population of agents following tournament selection.
+def test_returns_best_agent_and_new_population_multi_agent():
+    state_dim = [[4], [4]]
+    action_dim = [2, 2]
+    one_hot = False
+    net_config = {"arch": "mlp", "h_size": [8]}
+    population_size = 4
+    device = "cpu"
+    population_size = 5
+
+    # Initialize the class
+    tournament_selection = TournamentSelection(3, True, population_size, 2)
+
+    algo_classes = {"MADDPG": MADDPG, "MATD3": MATD3}
+
+    for algo in algo_classes.keys():
+        population = initialPopulation(
+            algo=algo,
+            state_dim=state_dim,
+            action_dim=action_dim,
+            one_hot=one_hot,
+            net_config=net_config,
+            INIT_HP=SHARED_INIT_HP,
+            population_size=population_size,
+            device=device,
+        )
+
+        population[0].fitness = [1, 2, 3]
+        population[1].fitness = [4, 5, 6]
+        population[2].fitness = [7, 8, 9]
+        population[3].fitness = [10, 11, 12]
+        population[4].fitness = [13, 14, 15]
+
+        # Call the select method
+        elite, new_population = tournament_selection.select(population)
+
+        # Check if the elite agent is the best agent in the population
+        assert elite.fitness == [13, 14, 15]
+        assert elite.index == 4
+        assert new_population[0].fitness == [13, 14, 15]
+        assert new_population[0].index == 4
+
+        # Check if the new population has the correct length
+        assert len(new_population) == population_size
+
+
+# Returns best agent and new population of agents following tournament selection without elitism.
+def test_returns_best_agent_and_new_population_without_elitism_multi_agent():
+    state_dim = [[4], [4]]
+    action_dim = [2, 2]
+    one_hot = False
+    net_config = {"arch": "mlp", "h_size": [8]}
+    population_size = 4
+    device = "cpu"
+    population_size = 5
+
+    # Initialize the class
+    tournament_selection = TournamentSelection(3, False, population_size, 2)
+
+    algo_classes = {"MADDPG": MADDPG, "MATD3": MATD3}
 
     for algo in algo_classes.keys():
         population = initialPopulation(
