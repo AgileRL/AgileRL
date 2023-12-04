a864b4fda4782f2e54434ee8a2c3c8755a6aa1b3
gonultasbu <gonultasbu@users.noreply.github.com>
2023-12-04 16:21:46 -0600

    tests for AutoResetWrapper on default PZ ParallelEnv

---
+++ tests/test_autoreset.py
@@ -0,0 +1,19 @@
+from agilerl.wrappers.pettingzoo_wrappers import PettingZooAutoResetParallelWrapper
+from testing_env import parallel_env
+
+
+def test_autoreset_wrapper_autoreset():
+    """Tests the autoreset wrapper actually automatically resets correctly."""
+    env = parallel_env(render_mode="human")
+    env = PettingZooAutoResetParallelWrapper(env)
+
+    observations, infos = env.reset()
+    for _ in range(500):
+        # this is where you would insert your policy
+        actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+        observations, rewards, terminations, truncations, infos = env.step(actions)
+        assert list(infos.values())[0] < 100, "Game went on for too long"
+
+
+if __name__ == "__main__":
+    test_autoreset_wrapper_autoreset()
