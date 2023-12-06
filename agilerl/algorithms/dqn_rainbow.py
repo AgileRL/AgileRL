c75282a3748471141b4333866f7bdd4d208d1b4c
mikepratt1 <michael.pratt160@gmail.com>
2023-12-06 16:26:18 +0000

    remove epsilon greedy from rainbow

--- agilerl/algorithms/dqn_rainbow.py
+++ agilerl/algorithms/dqn_rainbow.py
@@ -245,7 +245,7 @@ class RainbowDQN:
 
         self.criterion = nn.MSELoss()
 
-    def getAction(self, state, epsilon=0, action_mask=None):
+    def getAction(self, state, action_mask=None):
         """Returns the next action to take in the environment.
 
         :param state: State observation, or multiple observations in a batch
@@ -269,49 +269,19 @@ class RainbowDQN:
         if len(state.size()) < 2:
             state = state.unsqueeze(0)
 
-        # self.actor.eval()
-        # with torch.no_grad():
-        #     action_values = self.actor(state)
-        # self.actor.train()
+        self.actor.eval()
+        with torch.no_grad():
+            action_values = self.actor(state)
+        self.actor.train()
 
-        # if action_mask is None:
-        #     action = np.argmax(action_values.cpu().data.numpy(), axis=-1)
-        # else:
-        #     inv_mask = 1 - action_mask
-        #     masked_action_values = np.ma.array(
-        #         action_values.cpu().data.numpy(), mask=inv_mask
-        #     )
-        #     action = np.argmax(masked_action_values, axis=-1)
-
-        # return action
-        import random
-
-        # epsilon-greedy
-        if random.random() < epsilon:
-            if action_mask is None:
-                action = np.random.randint(0, self.action_dim, size=state.size()[0])
-            else:
-                inv_mask = 1 - action_mask
-
-                available_actions = np.ma.array(
-                    np.arange(0, self.action_dim), mask=inv_mask
-                ).compressed()
-                action = np.random.choice(available_actions, size=state.size()[0])
-
+        if action_mask is None:
+            action = np.argmax(action_values.cpu().data.numpy(), axis=-1)
         else:
-            self.actor.eval()
-            with torch.no_grad():
-                action_values = self.actor(state)
-            self.actor.train()
-
-            if action_mask is None:
-                action = np.argmax(action_values.cpu().data.numpy(), axis=-1)
-            else:
-                inv_mask = 1 - action_mask
-                masked_action_values = np.ma.array(
-                    action_values.cpu().data.numpy(), mask=inv_mask
-                )
-                action = np.argmax(masked_action_values, axis=-1)
+            inv_mask = 1 - action_mask
+            masked_action_values = np.ma.array(
+                action_values.cpu().data.numpy(), mask=inv_mask
+            )
+            action = np.argmax(masked_action_values, axis=-1)
 
         return action
 
