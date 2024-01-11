c751ce2574c7c3ece49e2e47f16c7f22658e2857
nicku-a <nickua@btinternet.com>
2024-01-11 11:13:32 +0000

    ILQL transformer updates and model saving and loading improvements

--- tests/test_ilql.py
+++ tests/test_ilql.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import torch
 
 from agilerl.algorithms.ilql import ILQL
@@ -314,3 +316,265 @@ def test_get_scores():
         items, exp_weights=True, clip_weight=0.1, include_logits=True
     )
     assert isinstance(score, torch.Tensor)
+
+    initial_score = algo.initial_score(items)
+
+    assert initial_score[0].shape == (1, 35)
+
+
+def test_soft_update():
+    List_RL_Dataset.__abstractmethods__ = set()
+
+    tokenizer = WordleTokenizer()
+    token_reward = ConstantTokenReward(1)
+    rl_ds = List_RL_Dataset(tokenizer, token_reward, 10)
+    net_config = {
+        "arch": "gpt",
+        "vocab_size": 12,
+        "n_layer": 2,
+        "n_embd": 12,
+        "n_head": 2,
+        "dim_feedfwd": 8,
+        "block_size": 8,
+        "activation": "GELU",
+        "dropout": 0.1,
+        "layer_norm_eps": 1e-5,
+        "min_layers": 8,
+        "max_layers": 16,
+        "bias": True,
+    }
+
+    algo = ILQL(rl_ds, net_config=net_config, double_q=True)
+
+    algo.softUpdate()
+
+    eval_params = list(algo.q.parameters())
+    target_params = list(algo.target_q.parameters())
+    expected_params = [
+        algo.alpha * eval_param + (1.0 - algo.alpha) * target_param
+        for eval_param, target_param in zip(eval_params, target_params)
+    ]
+
+    assert all(
+        torch.allclose(expected_param, target_param)
+        for expected_param, target_param in zip(expected_params, target_params)
+    )
+
+    eval_params = list(algo.q2.parameters())
+    target_params = list(algo.target_q2.parameters())
+    expected_params = [
+        algo.alpha * eval_param + (1.0 - algo.alpha) * target_param
+        for eval_param, target_param in zip(eval_params, target_params)
+    ]
+
+    assert all(
+        torch.allclose(expected_param, target_param)
+        for expected_param, target_param in zip(expected_params, target_params)
+    )
+
+    eval_params = list(algo.actor_target.parameters())
+    target_params = list(algo.model.parameters())
+    expected_params = [
+        algo.alpha * eval_param + (1.0 - algo.alpha) * target_param
+        for eval_param, target_param in zip(eval_params, target_params)
+    ]
+
+    assert all(
+        torch.allclose(expected_param, target_param)
+        for expected_param, target_param in zip(expected_params, target_params)
+    )
+
+
+def test_hard_update():
+    List_RL_Dataset.__abstractmethods__ = set()
+
+    tokenizer = WordleTokenizer()
+    token_reward = ConstantTokenReward(1)
+    rl_ds = List_RL_Dataset(tokenizer, token_reward, 10)
+    net_config = {
+        "arch": "gpt",
+        "vocab_size": 12,
+        "n_layer": 2,
+        "n_embd": 12,
+        "n_head": 2,
+        "dim_feedfwd": 8,
+        "block_size": 8,
+        "activation": "GELU",
+        "dropout": 0.1,
+        "layer_norm_eps": 1e-5,
+        "min_layers": 8,
+        "max_layers": 16,
+        "bias": True,
+    }
+
+    algo = ILQL(rl_ds, net_config=net_config, double_q=True)
+
+    algo.hardUpdate()
+
+    assert str(algo.q.state_dict()) == str(algo.target_q.state_dict())
+    assert str(algo.q2.state_dict()) == str(algo.target_q2.state_dict())
+    assert str(algo.actor_target.state_dict()) == str(algo.model.state_dict())
+
+
+def test_clone():
+    List_RL_Dataset.__abstractmethods__ = set()
+
+    tokenizer = WordleTokenizer()
+    token_reward = ConstantTokenReward(1)
+    rl_ds = List_RL_Dataset(tokenizer, token_reward, 10)
+    net_config = {
+        "arch": "gpt",
+        "vocab_size": 12,
+        "n_layer": 2,
+        "n_embd": 12,
+        "n_head": 2,
+        "dim_feedfwd": 8,
+        "block_size": 8,
+        "activation": "GELU",
+        "dropout": 0.1,
+        "layer_norm_eps": 1e-5,
+        "min_layers": 8,
+        "max_layers": 16,
+        "bias": True,
+    }
+
+    algo = ILQL(rl_ds, net_config=net_config, double_q=True)
+
+    clone = algo.clone()
+
+    assert algo.algo == clone.algo
+    assert algo.index == clone.index
+    assert algo.batch_size == clone.batch_size
+    assert algo.lr == clone.lr
+    assert algo.alpha == clone.alpha
+    assert algo.beta == clone.beta
+    assert algo.gamma == clone.gamma
+    assert algo.tau == clone.tau
+    assert algo.mut is clone.mut
+    assert algo.transition_weight == clone.transition_weight
+    assert algo.clip_weight is clone.clip_weight
+    assert algo.value_max is clone.value_max
+    assert algo.value_min is clone.value_min
+    assert algo.detach_v is clone.detach_v
+    assert algo.detach_q is clone.detach_q
+    assert algo.detach_pi is clone.detach_pi
+    assert algo.double_q is clone.double_q
+    assert algo.per_token is clone.per_token
+    assert algo.exp_weights is clone.exp_weights
+    assert algo.dm_margin == clone.dm_margin
+    assert algo.cql_temp == clone.cql_temp
+    assert algo.weight_decay == clone.weight_decay
+    assert algo.device == clone.device
+    assert str(algo.v.state_dict()) == str(clone.v.state_dict())
+    assert str(algo.pi.state_dict()) == str(clone.pi.state_dict())
+    assert str(algo.q.state_dict()) == str(clone.q.state_dict())
+    assert str(algo.target_q.state_dict()) == str(clone.target_q.state_dict())
+    assert str(algo.q2.state_dict()) == str(clone.q2.state_dict())
+    assert str(algo.target_q2.state_dict()) == str(clone.target_q2.state_dict())
+    assert str(algo.actor.state_dict()) == str(clone.actor.state_dict())
+    assert str(algo.actor_target.state_dict()) == str(clone.actor_target.state_dict())
+    assert str(algo.model.state_dict()) == str(clone.model.state_dict())
+
+
+def test_save_load_checkpoint(tmpdir):
+    List_RL_Dataset.__abstractmethods__ = set()
+
+    tokenizer = WordleTokenizer()
+    token_reward = ConstantTokenReward(1)
+    rl_ds = List_RL_Dataset(tokenizer, token_reward, 10)
+    net_config = {
+        "arch": "gpt",
+        "vocab_size": 12,
+        "n_layer": 2,
+        "n_embd": 12,
+        "n_head": 2,
+        "dim_feedfwd": 8,
+        "block_size": 8,
+        "activation": "GELU",
+        "dropout": 0.1,
+        "layer_norm_eps": 1e-5,
+        "min_layers": 8,
+        "max_layers": 16,
+        "bias": True,
+    }
+
+    algo = ILQL(rl_ds, net_config=net_config, double_q=True)
+
+    # Save the checkpoint to a file
+    checkpoint_path = Path(tmpdir) / "checkpoint.pth"
+    algo.saveCheckpoint(checkpoint_path)
+
+    checkpoint = torch.load(checkpoint_path)
+
+    assert "model_init_dict" in checkpoint
+    assert "model_state_dict" in checkpoint
+    assert "actor_init_dict" in checkpoint
+    assert "actor_state_dict" in checkpoint
+    assert "actor_target_init_dict" in checkpoint
+    assert "actor_target_state_dict" in checkpoint
+    assert "v_init_dict" in checkpoint
+    assert "v_state_dict" in checkpoint
+    assert "pi_init_dict" in checkpoint
+    assert "pi_state_dict" in checkpoint
+    assert "q_init_dict" in checkpoint
+    assert "q_state_dict" in checkpoint
+    assert "target_q_init_dict" in checkpoint
+    assert "target_q_state_dict" in checkpoint
+    assert "q2_init_dict" in checkpoint
+    assert "q2_state_dict" in checkpoint
+    assert "target_q2_init_dict" in checkpoint
+    assert "target_q2_state_dict" in checkpoint
+    assert "optimizer_state_dict" in checkpoint
+    assert "dataset" in checkpoint
+    assert "net_config" in checkpoint
+    assert "batch_size" in checkpoint
+    assert "lr" in checkpoint
+    assert "alpha" in checkpoint
+    assert "beta" in checkpoint
+    assert "gamma" in checkpoint
+    assert "tau" in checkpoint
+    assert "mutation" in checkpoint
+    assert "transition_weight" in checkpoint
+    assert "clip_weight" in checkpoint
+    assert "value_max" in checkpoint
+    assert "value_min" in checkpoint
+    assert "detach_v" in checkpoint
+    assert "detach_q" in checkpoint
+    assert "detach_pi" in checkpoint
+    assert "double_q" in checkpoint
+    assert "per_token" in checkpoint
+    assert "exp_weights" in checkpoint
+    assert "dm_margin" in checkpoint
+    assert "cql_temp" in checkpoint
+    assert "weight_decay" in checkpoint
+    assert "index" in checkpoint
+    assert "scores" in checkpoint
+    assert "fitness" in checkpoint
+    assert "steps" in checkpoint
+
+    algo = ILQL(rl_ds, net_config=net_config, double_q=True)
+    algo.loadCheckpoint(checkpoint_path)
+
+    assert algo.algo == "ILQL"
+    assert algo.index == 0
+    assert algo.batch_size == 64
+    assert algo.lr == 1e-5
+    assert algo.alpha == 0.005
+    assert algo.beta == 0.0
+    assert algo.gamma == 0.99
+    assert algo.tau == 0.6
+    assert algo.mut is None
+    assert algo.transition_weight == 0.0
+    assert algo.clip_weight is None
+    assert algo.value_max is None
+    assert algo.value_min is None
+    assert algo.detach_v is False
+    assert algo.detach_q is False
+    assert algo.detach_pi is False
+    assert algo.double_q is True
+    assert algo.per_token is True
+    assert algo.exp_weights is True
+    assert algo.dm_margin == 0.0
+    assert algo.cql_temp == 1.0
+    assert algo.weight_decay == 0.0
+    assert algo.device == "cpu"
