fb9f0798f12f9c2118892726c22af5f7e5b49551
nicku-a <nickua@btinternet.com>
2024-02-19 16:47:39 +0000

    Reinitialize exploration layer after mutation

--- agilerl/hpo/mutation.py
+++ agilerl/hpo/mutation.py
@@ -553,6 +553,16 @@ class Mutations:
                         individual, critic["eval"], offspring_critic.to(self.device)
                     )
 
+            if individual.algo in ["NeuralUCB", "NeuralTS"]:
+                if self.arch == "mlp":
+                    individual.exp_layer = (
+                        individual.actor.feature_net.linear_layer_output
+                    )
+                else:
+                    individual.exp_layer = (
+                        individual.actor.value_net.value_linear_layer_output
+                    )
+
         self.reinit_opt(individual)  # Reinitialise optimizer
         individual.mut = "act"
         return individual
@@ -850,8 +860,11 @@ class Mutations:
 
         else:
             if individual.algo in ["NeuralUCB", "NeuralTS"]:
-                self.new_layer_prob = 0
                 old_actor = getattr(individual, self.algo["actor"]["eval"]).clone()
+                if self.arch == "mlp":
+                    old_exp_layer = old_actor.feature_net.linear_layer_output
+                else:
+                    old_exp_layer = old_actor.value_net.value_linear_layer_output
 
             offspring_actor = getattr(individual, self.algo["actor"]["eval"]).clone()
             offspring_critics = [
@@ -966,76 +979,84 @@ class Mutations:
                     )
 
             if individual.algo in ["NeuralUCB", "NeuralTS"]:
-                individual.numel = sum(
-                    w.numel() for w in offspring_actor.parameters() if w.requires_grad
-                )
-                new_sigma_inv = individual.lamb * np.eye(individual.numel)
-                individual.theta_0 = torch.cat(
-                    [
-                        w.flatten()
-                        for w in offspring_actor.parameters()
-                        if w.requires_grad
-                    ]
-                )
-
-                # create matrix that is copy of sigma inv
-                # first go through old params, figure out which to remove, then remove any difference
-                # then go through new params, figure out where to add, then add zeros/lambda
-                new_sigma_inv = copy.deepcopy(individual.sigma_inv)
-                old_params = dict(old_actor.named_parameters())
-                new_params = dict(offspring_actor.named_parameters())
-
-                to_remove = []
-                i = 0
-                for key, param in old_actor.named_parameters():
-                    if param.requires_grad:
-                        old_size = param.numel()
-                        if key not in new_params.keys():
-                            to_remove += list(range(i, i + old_size))
-                        else:
-                            new_size = new_params[key].numel()
-                            if new_size < old_size:
-                                to_remove += list(range(i + new_size, i + old_size))
-                        i += old_size
-
-                to_add = []
-                i = 0
-                for key, param in offspring_actor.named_parameters():
-                    if param.requires_grad:
-                        new_size = param.numel()
-                        if key in old_params.keys():
-                            old_size = old_params[key].numel()
-                            if new_size > old_size:
-                                to_add += list(range(i + old_size, i + new_size))
-                        else:
-                            to_add += list(range(i, i + new_size))
-                        i += new_size
-
-                # Adjust indixes to add after deletion
-                to_remove = np.array(to_remove)
-                to_add = np.array(to_add)
-                to_add -= np.sum(to_add[:, np.newaxis] > to_remove, axis=1)
-                to_add -= np.arange(len(to_add))
-
-                # Remove elements corresponding to old params
-                if len(to_remove) > 0:
-                    new_sigma_inv = np.delete(
-                        np.delete(new_sigma_inv, to_remove, 0), to_remove, 1
-                    )
-
-                # Add new zeros corresponding to new params, make lambda down identity diagonal
-                if len(to_add) > 0:
-                    new_sigma_inv = np.insert(
-                        np.insert(new_sigma_inv, to_add, 0, 0), to_add, 0, 1
-                    )
-                    for i in to_add:
-                        new_sigma_inv[i, i] = individual.lamb
-
-                individual.sigma_inv = new_sigma_inv
+                self._reinit_bandit_grads(individual, offspring_actor, old_exp_layer)
 
         self.reinit_opt(individual)  # Reinitialise optimizer
         individual.mut = "arch"
         return individual
+
+    def _reinit_bandit_grads(self, individual, offspring_actor, old_exp_layer):
+        if self.arch == "mlp":
+            exp_layer = offspring_actor.feature_net.linear_layer_output
+        else:
+            exp_layer = offspring_actor.value_net.value_linear_layer_output
+
+        individual.numel = sum(
+            w.numel() for w in exp_layer.parameters() if w.requires_grad
+        )
+        individual.theta_0 = torch.cat(
+            [w.flatten() for w in exp_layer.parameters() if w.requires_grad]
+        )
+
+        # create matrix that is copy of sigma inv
+        # first go through old params, figure out which to remove, then remove any difference
+        # then go through new params, figure out where to add, then add zeros/lambda
+        new_sigma_inv = copy.deepcopy(individual.sigma_inv).cpu().numpy()
+        old_params = dict(old_exp_layer.named_parameters())
+        new_params = dict(exp_layer.named_parameters())
+
+        to_remove = []
+        i = 0
+        for key, param in old_exp_layer.named_parameters():
+            if param.requires_grad:
+                old_size = param.numel()
+                if key not in new_params.keys():
+                    to_remove += list(range(i, i + old_size))
+                else:
+                    new_size = new_params[key].numel()
+                    if new_size < old_size:
+                        to_remove += list(range(i + new_size, i + old_size))
+                i += old_size
+
+        to_add = []
+        i = 0
+        for key, param in exp_layer.named_parameters():
+            if param.requires_grad:
+                new_size = param.numel()
+                if key in old_params.keys():
+                    old_size = old_params[key].numel()
+                    if new_size > old_size:
+                        to_add += list(range(i + old_size, i + new_size))
+                else:
+                    to_add += list(range(i, i + new_size))
+                i += new_size
+
+        # Adjust indixes to add after deletion
+        to_remove = np.array(to_remove)
+        to_add = np.array(to_add)
+        to_add -= np.sum(to_add[:, np.newaxis] > to_remove, axis=1)
+        to_add -= np.arange(len(to_add))
+
+        # Remove elements corresponding to old params
+        if len(to_remove) > 0:
+            new_sigma_inv = np.delete(
+                np.delete(new_sigma_inv, to_remove, 0), to_remove, 1
+            )
+
+        # Add new zeros corresponding to new params, make lambda down identity diagonal
+        if len(to_add) > 0:
+            new_sigma_inv = np.insert(
+                np.insert(new_sigma_inv, to_add, 0, 0), to_add, 0, 1
+            )
+            for i in to_add:
+                new_sigma_inv[i, i] = individual.lamb
+
+        individual.exp_layer = exp_layer
+        individual.sigma_inv = torch.from_numpy(new_sigma_inv).to(
+            individual.device
+            if individual.accelerator is None
+            else individual.accelerator.device
+        )
 
     def get_algo_nets(self, algo):
         """Returns dictionary with agent network names.
