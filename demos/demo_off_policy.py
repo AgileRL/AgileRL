d972cbafdb5e7e70b9fdcc82e0debf48b1431053
nicku-a <nickua@btinternet.com>
2024-06-19 14:33:01 +0100

    Update demo off policy with step-based episodes

---
+++ demos/demo_off_policy.py
@@ -0,0 +1,220 @@
+import numpy as np
+import torch
+from tqdm import trange
+
+from agilerl.components.replay_buffer import ReplayBuffer
+from agilerl.hpo.mutation import Mutations
+from agilerl.hpo.tournament import TournamentSelection
+from agilerl.utils.utils import initialPopulation, makeVectEnvs
+
+# !Note: If you are running this demo without having installed agilerl,
+# uncomment and place the following above agilerl imports:
+
+# import sys
+# sys.path.append('../')
+
+
+if __name__ == "__main__":
+    print("===== AgileRL Off-policy Demo =====")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    NET_CONFIG = {
+        "arch": "mlp",  # Network architecture
+        "hidden_size": [32, 32],  # Actor hidden size
+    }
+
+    INIT_HP = {
+        "DOUBLE": True,  # Use double Q-learning
+        "BATCH_SIZE": 128,  # Batch size
+        "LR": 1e-3,  # Learning rate
+        "GAMMA": 0.99,  # Discount factor
+        "LEARN_STEP": 1,  # Learning frequency
+        "TAU": 1e-3,  # For soft update of target network parameters
+        "CHANNELS_LAST": False,  # Swap image channels dimension last to first [H, W, C] -> [C, H, W]
+        "POP_SIZE": 4,  # Population size
+    }
+
+    num_envs = 16
+    env = makeVectEnvs("LunarLander-v2", num_envs=num_envs)  # Create environment
+
+    try:
+        state_dim = env.single_observation_space.n  # Discrete observation space
+        one_hot = True  # Requires one-hot encoding
+    except Exception:
+        state_dim = env.single_observation_space.shape  # Continuous observation space
+        one_hot = False  # Does not require one-hot encoding
+    try:
+        action_dim = env.single_action_space.n  # Discrete action space
+    except Exception:
+        action_dim = env.single_action_space.shape[0]  # Continuous action space
+
+    if INIT_HP["CHANNELS_LAST"]:
+        state_dim = (state_dim[2], state_dim[0], state_dim[1])
+
+    pop = initialPopulation(
+        algo="DQN",  # Algorithm
+        state_dim=state_dim,  # State dimension
+        action_dim=action_dim,  # Action dimension
+        one_hot=one_hot,  # One-hot encoding
+        net_config=NET_CONFIG,  # Network configuration
+        INIT_HP=INIT_HP,  # Initial hyperparameters
+        population_size=INIT_HP["POP_SIZE"],  # Population size
+        num_envs=num_envs,  # Number of vectorized envs
+        device=device,
+    )
+
+    field_names = ["state", "action", "reward", "next_state", "done"]
+    memory = ReplayBuffer(
+        memory_size=10000,  # Max replay buffer size
+        field_names=field_names,  # Field names to store in memory
+        device=device,
+    )
+
+    tournament = TournamentSelection(
+        tournament_size=2,  # Tournament selection size
+        elitism=True,  # Elitism in tournament selection
+        population_size=INIT_HP["POP_SIZE"],  # Population size
+        eval_loop=1,  # Evaluate using last N fitness scores
+    )
+
+    mutations = Mutations(
+        algo="DQN",  # Algorithm
+        no_mutation=0.4,  # No mutation
+        architecture=0.2,  # Architecture mutation
+        new_layer_prob=0.2,  # New layer mutation
+        parameters=0.2,  # Network parameters mutation
+        activation=0,  # Activation layer mutation
+        rl_hp=0.2,  # Learning HP mutation
+        rl_hp_selection=["lr", "batch_size"],  # Learning HPs to choose from
+        mutation_sd=0.1,  # Mutation strength
+        arch=NET_CONFIG["arch"],  # Network architecture
+        rand_seed=1,  # Random seed
+        device=device,
+    )
+
+    max_steps = 200000  # Max steps
+    learning_delay = 1000  # Steps before starting learning
+
+    # Exploration params
+    eps_start = 1.0  # Max exploration
+    eps_end = 0.1  # Min exploration
+    eps_decay = 0.995  # Decay per episode
+    epsilon = eps_start
+
+    evo_steps = 10000  # Evolution frequency
+    eval_steps = None  # Evaluation steps per episode - go until done
+    eval_loop = 1  # Number of evaluation episodes
+
+    total_steps = 0
+
+    pbar = trange(max_steps, unit="step")
+
+    # TRAINING LOOP
+    print("Training...")
+    while np.less([agent.steps[-1] for agent in pop], max_steps).all():
+        pop_episode_scores = []
+        for agent in pop:  # Loop through population
+            state, info = env.reset()  # Reset environment at start of episode
+            scores = np.zeros(num_envs)
+            completed_episode_scores, losses = [], []
+            steps = 0
+            epsilon = eps_start
+
+            for idx_step in range(evo_steps // num_envs):
+                if INIT_HP["CHANNELS_LAST"]:
+                    state = np.moveaxis(state, [3], [1])
+
+                action = agent.getAction(state, epsilon)  # Get next action from agent
+                epsilon = max(
+                    eps_end, epsilon * eps_decay
+                )  # Decay epsilon for exploration
+
+                # Act in environment
+                next_state, reward, terminated, truncated, info = env.step(action)
+                scores += np.array(reward)
+                steps += num_envs
+                total_steps += num_envs
+
+                # Collect scores for completed episodes
+                for idx, (d, t) in enumerate(zip(terminated, truncated)):
+                    if d or t:
+                        completed_episode_scores.append(scores[idx])
+                        agent.scores.append(scores[idx])
+                        scores[idx] = 0
+
+                # Save experience to replay buffer
+                if INIT_HP["CHANNELS_LAST"]:
+                    memory.save2memory(
+                        state,
+                        action,
+                        reward,
+                        np.moveaxis(next_state, [3], [1]),
+                        terminated,
+                        is_vectorised=True,
+                    )
+                else:
+                    memory.save2memory(
+                        state,
+                        action,
+                        reward,
+                        next_state,
+                        terminated,
+                        is_vectorised=True,
+                    )
+
+                # Learn according to learning frequency
+                if memory.counter > learning_delay and len(memory) >= agent.batch_size:
+                    for _ in range(num_envs // agent.learn_step):
+                        experiences = memory.sample(
+                            agent.batch_size
+                        )  # Sample replay buffer
+                        agent.learn(
+                            experiences
+                        )  # Learn according to agent's RL algorithm
+
+                state = next_state
+
+            pbar.update(evo_steps // len(pop))
+            agent.steps[-1] += steps
+            pop_episode_scores.append(completed_episode_scores)
+
+        # Reset epsilon start to latest decayed value for next round of population training
+        eps_start = epsilon
+
+        # Evaluate population
+        fitnesses = [
+            agent.test(
+                env,
+                swap_channels=INIT_HP["CHANNELS_LAST"],
+                max_steps=eval_steps,
+                loop=eval_loop,
+            )
+            for agent in pop
+        ]
+        mean_scores = [
+            (
+                np.mean(episode_scores)
+                if len(episode_scores) > 0
+                else "0 completed episodes"
+            )
+            for episode_scores in pop_episode_scores
+        ]
+
+        print(f"--- Global steps {total_steps} ---")
+        print(f"Steps {[agent.steps[-1] for agent in pop]}")
+        print(f"Scores: {mean_scores}")
+        print(f'Fitnesses: {["%.2f"%fitness for fitness in fitnesses]}')
+        print(
+            f'5 fitness avgs: {["%.2f"%np.mean(agent.fitness[-5:]) for agent in pop]}'
+        )
+
+        # Tournament selection and population mutation
+        elite, pop = tournament.select(pop)
+        pop = mutations.mutation(pop)
+
+        # Update step counter
+        for agent in pop:
+            agent.steps.append(agent.steps[-1])
+
+    env.close()
