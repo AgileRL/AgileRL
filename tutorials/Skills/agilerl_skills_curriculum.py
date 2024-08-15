55fd54733d2035bddd2835a2615b29be98ad540a
Michael Pratt <118982716+mikepratt1@users.noreply.github.com>
2024-08-15 15:11:56 +0100

    Update agilerl_skills_curriculum.py, remove + 1 for printing progress

--- tutorials/Skills/agilerl_skills_curriculum.py
+++ tutorials/Skills/agilerl_skills_curriculum.py
@@ -349,7 +349,7 @@ if __name__ == "__main__":
             agent.steps[-1] += idx_step + 1
             total_steps += idx_step + 1
 
-        if (agent.steps[-1] + 1) % INIT_HP["EVO_STEPS"] == 0:
+        if (agent.steps[-1]) % INIT_HP["EVO_STEPS"] == 0:
             mean_scores = np.mean([agent.scores[-20:] for agent in pop], axis=1)
             if INIT_HP["WANDB"]:
                 wandb.log(
