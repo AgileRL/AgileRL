59bf7e617a3ca10e2f3ae8fed1f17703fc396b35
mikepratt1 <michael.pratt160@gmail.com>
2024-11-13 10:47:03 +0000

    change timeout to 60 seconds

--- agilerl/vector/pz_async_vec_env.py
+++ agilerl/vector/pz_async_vec_env.py
@@ -205,7 +205,7 @@ class AsyncPettingZooVecEnv(PettingZooVecEnv):
             pipe.send(("reset", env_kwargs))
         self._state = AsyncState.WAITING_RESET
 
-    def reset_wait(self, timeout=10):
+    def reset_wait(self, timeout=60):
         """Waits for the calls triggered by :meth:`reset_async` to finish and returns the results.
 
         :param timeout: Number of seconds before the call to ``reset_wait`` times out. If `None`, the call to ``reset_wait`` never times out, defaults to 0
@@ -266,7 +266,7 @@ class AsyncPettingZooVecEnv(PettingZooVecEnv):
             pipe.send(("step", action))
         self._state = AsyncState.WAITING_STEP
 
-    def step_wait(self, timeout=10):
+    def step_wait(self, timeout=60):
         """
         Wait for the calls to :obj:`step` in each sub-environment to finish.
 
@@ -363,7 +363,7 @@ class AsyncPettingZooVecEnv(PettingZooVecEnv):
             pipe.send(("_call", (name, args, kwargs)))
         self._state = AsyncState.WAITING_CALL
 
-    def call_wait(self, timeout=10):
+    def call_wait(self, timeout=60):
         """Calls all parent pipes and waits for the results.
 
         :param timeout: Number of seconds before the call to :meth:`call_wait` times out. If ``None`` (default), the call to :meth:`call_wait` never times out, defaults to 0
@@ -426,7 +426,7 @@ class AsyncPettingZooVecEnv(PettingZooVecEnv):
         _, successes = zip(*[pipe.recv() for pipe in self.parent_pipes])
         self._raise_if_errors(successes)
 
-    def close_extras(self, timeout=10, terminate=False):
+    def close_extras(self, timeout=60, terminate=False):
         """
         Close the environments & clean up the extra resources (processes and pipes).
 
