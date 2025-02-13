Action masking fixes

agilerl/algorithms/dqn.py
@@ -211,7 +211,7 @@ class DQN(RLAlgorithm):
             # Need to stack if vectorized env
             action_mask = (
                 np.stack(action_mask)
-                if action_mask.dtype == np.object_
+                if action_mask.dtype == np.object_ or isinstance(action_mask, list)
                 else action_mask
             )
             action_mask = torch.as_tensor(action_mask, device=device)

agilerl/algorithms/dqn_rainbow.py
@@ -251,7 +251,7 @@ class RainbowDQN(RLAlgorithm):
             # Need to stack if vectorized env
             action_mask = (
                 np.stack(action_mask)
-                if action_mask.dtype == np.object_
+                if action_mask.dtype == np.object_ or isinstance(action_mask, list)
                 else action_mask
             )
             inv_mask = 1 - action_mask

agilerl/networks/actors.py
@@ -112,7 +112,7 @@ class EvolvableDistribution(EvolvableWrapper):
             if isinstance(action_mask, np.ndarray):
                 action_mask = (
                     np.stack(action_mask)
-                    if action_mask.dtype == np.object_
+                    if action_mask.dtype == np.object_ or isinstance(action_mask, list)
                     else action_mask
                 )
 