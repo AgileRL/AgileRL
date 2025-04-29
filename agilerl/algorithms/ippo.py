f9f2068c712e1927594752bd42e599bbf6faad38
Jaime Sabal <jsabalb@gmail.com>
2025-04-29 13:44:41 +0100

    Progress toward general PzAsyncVecEnv and IPPO

--- agilerl/algorithms/ippo.py
+++ agilerl/algorithms/ippo.py
@@ -440,8 +440,8 @@ class IPPO(MultiAgentRLAlgorithm):
             else:
                 sample = agent_obs
 
-            # Extract rows where all values are NaN
-            inactive_agent_indices = np.where(np.isnan(sample).all(axis=1))[0]
+            # Extract rows where all values are -1
+            inactive_agent_indices = np.where(np.all(sample == -1, axis=1))[0]
 
             # If agent is active in all environments, don't need to save info
             if inactive_agent_indices.shape[0] == 0:
@@ -486,7 +486,7 @@ class IPPO(MultiAgentRLAlgorithm):
         vect_dim = get_vect_dim(obs, self.observation_space)
 
         # Need to extract inactive agents from obs
-        # inactive_agents, obs = self.extract_inactive_agents(obs)
+        inactive_agents, obs = self.extract_inactive_agents(obs)
 
         # Preprocess observations
         unique_agents_ids = list(obs.keys())
@@ -573,19 +573,17 @@ class IPPO(MultiAgentRLAlgorithm):
             action_dict, vect_dim, homogenous_agents
         )
 
-        # Place NaNs in actions for inactive agents
-        # NOTE: With the current implementation of `AsyncPettingZooVecEnv`
-        # this is never used -> will need to take a second look in the future
-        # for agent_id, inactive_array in inactive_agents.items():
-        #     # Handle different action types (integers, floats, etc.)
-        #     placeholder = (
-        #         np.array([-999], dtype=action_dict[agent_id].dtype)
-        #         if np.issubdtype(action_dict[agent_id].dtype, np.integer)
-        #         else np.nan
-        #     )
-        #     action_dict[agent_id] = np.insert(
-        #         action_dict[agent_id], inactive_array, placeholder, axis=0
-        #     )
+        # Need to fill in placeholder values for inactive agents
+        for agent_id, inactive_array in inactive_agents.items():
+            # Handle different action types (integers, floats, etc.)
+            placeholder = (
+                np.array([-999], dtype=action_dict[agent_id].dtype)
+                if np.issubdtype(action_dict[agent_id].dtype, np.integer)
+                else np.nan
+            )
+            action_dict[agent_id] = np.insert(
+                action_dict[agent_id], inactive_array, placeholder, axis=0
+            )
 
         # If using env_defined_actions replace actions
         if env_defined_actions is not None:
