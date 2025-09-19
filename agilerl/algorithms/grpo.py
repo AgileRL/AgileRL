4fde08b6cfdfb3cf60588b6edf3114ab1e8972d2
Michael Pratt <mike@agilerl.com>
2025-09-19 13:27:02 +0100

    Print shapes of batch model kwargs

--- agilerl/algorithms/grpo.py
+++ agilerl/algorithms/grpo.py
@@ -859,6 +859,11 @@ class GRPO(LLMAlgorithm):
                 if self.calc_position_embeddings:
                     batch_position_ids = position_ids[batch:end_idx, :]
                     batch_model_kwargs |= {"position_ids": batch_position_ids}
+                if not eval_mode and self.accelerator.is_main_process:
+                    print(
+                        "Shape of forward input",
+                        {k: v.shape for k, v in batch_model_kwargs.items()},
+                    )
                 logits = self.actor.forward(**batch_model_kwargs).logits
                 logits = logits / self.temperature
                 if eval_mode and self.accelerator.is_main_process:
@@ -872,7 +877,7 @@ class GRPO(LLMAlgorithm):
                 batch_model_kwargs = None
                 logits = None
                 if not eval_mode:
-                    print("Log probs", log_prob)
+                    # print("Log probs", log_prob)
                     assert log_prob.requires_grad  # FIXME remove
                 log_probs.append(log_prob)
         return torch.cat(log_probs, dim=0)
