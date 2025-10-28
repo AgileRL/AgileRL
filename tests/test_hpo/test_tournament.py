3689b5e1b8b11aefd4c707104ac38b47ee427505
Michael Pratt <mike@agilerl.com>
2025-10-28 09:29:53 +0000

    Fix tournament tests

--- tests/test_hpo/test_tournament.py
+++ tests/test_hpo/test_tournament.py
@@ -326,7 +326,7 @@ def test_language_model_tournament(use_accelerator, elitism, num_processes):
         GRPO(
             observation_space=observation_space,
             action_space=action_space,
-            actor_network=clone_llm(actor_network),
+            actor_network=clone_llm(actor_network, 0),
             pad_token_id=INIT_HP.get("PAD_TOKEN_ID"),
             pad_token="<pad>",
             hp_config=None,
