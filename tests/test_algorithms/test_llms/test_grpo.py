ea4e2759b2251fcbe27ec5c54499dce021a888a2
Michael Pratt <mike@agilerl.com>
2025-12-10 14:20:18 +0000

    Change gpu utilization back to 0.05

--- tests/test_algorithms/test_llms/test_grpo.py
+++ tests/test_algorithms/test_llms/test_grpo.py
@@ -305,7 +305,7 @@ def grpo_factory():
         if use_vllm:
             lora_config = None
             vllm_config = VLLMConfig(
-                gpu_memory_utilization=0.2, max_num_seqs=1, sleep_mode=sleep_mode
+                gpu_memory_utilization=0.05, max_num_seqs=1, sleep_mode=sleep_mode
             )
 
             actor = model_factory(pretrained_model_name_or_path)
