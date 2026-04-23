62eb883d7dd04f7dda308420e96eb0239366c10e
micdoh <mic.d.doh@gmail.com>
2026-04-23 14:32:01 +0100

    Fix LLm import bug in algorithms/__init__.py

--- agilerl/algorithms/__init__.py
+++ agilerl/algorithms/__init__.py
@@ -1,13 +1,10 @@
 from agilerl import HAS_LLM_DEPENDENCIES
 
 from .bc_lm import BC_LM, BC_Evaluator, BC_Policy
-from .cispo import CISPO
 from .cqn import CQN
 from .ddpg import DDPG
 from .dqn import DQN
 from .dqn_rainbow import RainbowDQN
-from .grpo import GRPO
-from .gspo import GSPO
 from .ilql import ILQL
 from .ippo import IPPO
 from .maddpg import MADDPG
@@ -15,8 +12,6 @@ from .matd3 import MATD3
 from .neural_ts_bandit import NeuralTS
 from .neural_ucb_bandit import NeuralUCB
 from .ppo import PPO
-from .ppo_llm import PPO as LLMPPO
-from .reinforce_llm import REINFORCE as LLMReinforce
 from .td3 import TD3
 
 __all__ = [
