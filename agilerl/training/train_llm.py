be34c304d6caa45f2661274b6cf07de4dade4a74
Michael Pratt <mike@agilerl.com>
2026-07-06 14:55:40 +0100

    Fix pre commit

--- agilerl/training/train_llm.py
+++ agilerl/training/train_llm.py
@@ -7,10 +7,10 @@ from typing import Any
 
 import numpy as np
 import torch
-import wandb
 from accelerate import Accelerator
 from tqdm import trange
 
+import wandb
 from agilerl.algorithms import DPO, GRPO, LLMPPO, LLMREINFORCE, SFT
 from agilerl.hpo.mutation import Mutations
 from agilerl.hpo.tournament import TournamentSelection
